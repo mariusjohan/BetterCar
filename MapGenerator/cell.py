4aabc8787d704822da58fec717055d22a03924da
joha7809 <joha7809@gmail.com>
2022-11-09 22:13:41 +0100

    WIP

--- MapGenerator/cell.py
+++ MapGenerator/cell.py
@@ -1,4 +1,4 @@
-class Grid:
+class Cell:
     """TESTESTEST"""
     def __init__(self, row_id, col_id, total_row=16, total_col=9) -> None:
         self.row_id = row_id
@@ -9,10 +9,14 @@ class Grid:
         self.total_row = total_row
         self.total_col = total_col
 
-    def knock_down_wall(self, neighbour) -> None:
+    def knock_down_wall(self, neighbour: "Cell") -> None:
         wall_between = self.get_wall_between(neighbour)
-        self.walls[wall_between[0]] = False
+        print(f"Cell pos: {self.row_id, self.col_id} Neighbour pos: {neighbour.row_id, neighbour.col_id}")
+        print(wall_between)
+        wall_between_0 = wall_between[0]
+        self.walls[wall_between_0] = False
         neighbour.walls[wall_between[1]] = False
+        neighbour.visited = True
     
     def get_opening(self) -> list:
         "Returns list of directions of the openins of the cell-walls"
@@ -22,22 +26,23 @@ class Grid:
                 openings.append(key)
         return openings
 
-    def get_wall_between(self, neighbour) -> list:
+    def get_wall_between(self, neighbour: "Cell") -> list:
+        "Returns the direction of the wall, first for the current cell, and afterwards for the neighbour"
         if self.row_id == neighbour.row_id: #The wall must be above or below current cell
             if self.col_id - neighbour.col_id == -1: #The wall is above the current cell
                 return ['top', 'down']
             elif self.col_id - neighbour.col_id == 1: #The wall is below the current cell
                 return ['down', 'top']
+            print('2')
         if self.col_id == neighbour.col_id:
             if self.row_id - neighbour.row_id == -1: #The wall is to the right of current cell
                 return ['right', 'left']
             elif self.row_id - neighbour.row_id == 1: #The wall is to the left of current cell
                 return ['left', 'right']
+            print('3')
 
-    def get_neighbours(self) -> list:
-        if self.row_id == 0:
-            if self.col_id == 0:
-                return [[self.row_id+1, self.col_id], [self.row_id, self.col_id+1]]
-            if self.col_id == self.total_col:
-                pass
+        print('1')
+
+    def has_openings(self) -> bool:
+        return not (self.walls['top'] and self.walls['right'] and self.walls['down'] and self.walls['left'])
     
